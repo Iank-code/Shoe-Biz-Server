460caad1a59949fb4a76b1ea69513e80c0a6697f
Ian Kamau <kian99564@gmail.com>
2024-01-06 23:37:37 +0300

    Setting up server

---
+++ src/app/application/utils/helpers.ts
@@ -0,0 +1,39 @@
+import cors from "cors";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
+
+/**
+ * Helps to configure cors options based on the allowed domains
+ * that are passed on through the environment variables
+ * @returns
+ */
+const createCorsOptions = (): cors.CorsOptions => {
+  const allowedDomains = process.env.ALLOWED_DOMAINS ?? "*";
+  const domains = allowedDomains.split(";").join(",");
+
+  const corsOptions: cors.CorsOptions = {
+    origin: domains,
+  };
+  return corsOptions;
+};
+
+// For sending email
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.GMAIL_NAME,
+    pass: process.env.GMAIL_PASS,
+  },
+});
+
+export {
+  //   comparePasswords,
+  //   generateSecurePasswords,
+  createCorsOptions,
+  transporter,
+  //   generateRandomString,
+  //   formatAudioName,
+};
---
+++ src/app/presentation/rest/routes.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+// import CourseModule from "./modules/course.module";
+// import ApplicationModule from "./modules/application.module";
+// import GetInTouchModule from "./modules/GetInTouch.module";
+// import ContactModule from "./modules/contact.module";
+// import AdminModule from "./modules/admin.module";
+// import MpesaModule from "./modules/mpesa.module";
+
+export default class AppRouter {
+  public router: Router;
+
+  constructor() {
+    this.router = Router();
+    this.config();
+  }
+
+  private config() {
+    // this.router.use("/course", new CourseModule().router);
+    // this.router.use("/admin", new AdminModule().router);
+    // this.router.use("/mpesa", new MpesaModule().router);
+    // this.router.use("/application", new ApplicationModule().router);
+    // this.router.use("/contact", new ContactModule().router);
+    // this.router.use("/email/getintouch", new GetInTouchModule().router);
+  }
+}
