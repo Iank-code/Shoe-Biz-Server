4c83994c48cc3eac533ad2c63e36eb2edd62f403
Ian Kamau <kian99564@gmail.com>
2024-05-03 17:09:45 +0300

    feat: configuring mpesa .env, module, usecase and repository

--- src/app/presentation/rest/routes.ts
+++ src/app/presentation/rest/routes.ts
@@ -1,13 +1,11 @@
 import { Router } from "express";
 import ProductModule from "./modules/product.module";
-// import ApplicationModule from "./modules/application.module";
-// import GetInTouchModule from "./modules/GetInTouch.module";
 import OrderModule from "./modules/order.module";
 import customerModule from "./modules/customer.module";
 import sellerModule from "./modules/seller.module";
 import NewsLetterModule from "./modules/newsLetter.module";
 import analyticModule from "./modules/analytic.module";
-// import MpesaModule from "./modules/mpesa.module";
+import MpesaModule from "./modules/mpesa.module";
 
 export default class AppRouter {
   public router: Router;
@@ -22,10 +20,8 @@ export default class AppRouter {
     this.router.use("/customer", new customerModule().router);
     this.router.use("/seller", new sellerModule().router);
     this.router.use("/newsletter", new NewsLetterModule().router);
-    // this.router.use("/mpesa", new MpesaModule().router);
-    // this.router.use("/application", new ApplicationModule().router);
+    this.router.use("/mpesa", new MpesaModule().router);
     this.router.use("/order", new OrderModule().router);
-    // this.router.use("/email/getintouch", new GetInTouchModule().router);
     this.router.use("/analytic", new analyticModule().router);
   }
 }
